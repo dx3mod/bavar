5e6698de4d85bcf6c6aef69d1e32e46aaac43c23
dx3mod <dx3mod@bk.ru>
2024-05-29 19:13:56 +0300

    feat: add basic 'init' command

---
+++ bin/build_cmd.ml
@@ -0,0 +1,47 @@
+open Core
+open Bavar
+
+let current_path = Core_unix.getcwd ()
+
+let compile_the_project ~root_dir ~target () =
+  let config_file_path = Filename.concat root_dir "LabAvrProject" in
+
+  if Sys_unix.file_exists_exn config_file_path then (
+    printf "target: %s\nroot_dir: %s\n"
+      (Option.value target ~default:"none")
+      root_dir;
+
+    Stdlib.print_newline ();
+
+    try
+      let config = Project_config.of_file config_file_path in
+      let build_context = Build_context.make ~root_dir ~config in
+
+      let avr_project = Resolver.resolve_avr_project build_context in
+
+      Builder.build build_context avr_project Builder.Release
+      |> List.iter ~f:(fun u ->
+             Array.iter ~f:(Printf.printf "%s ") u;
+             print_endline "\n")
+    with
+    | Bavar.Project_config.Read_error err ->
+        Printf.eprintf "Failed to read config: %s.\nAt '%s' file.\n" err.message
+          err.path;
+        exit 1
+    | Sys_error e ->
+        Printf.eprintf "Sys_error. %s.\n" e;
+        exit 1)
+  else (
+    eprintf "Not found 'LabAvrProject' configuration file at %s.\n"
+      (if Filename.equal root_dir current_path then "current directory"
+       else root_dir);
+    exit 1)
+
+let command =
+  Command.basic ~summary:"compile the current project"
+    (let%map_open.Command root_dir =
+       flag "-root-dir"
+         (optional_with_default current_path string)
+         ~doc:"path to project"
+     and target = anon @@ maybe ("target" %: string) in
+     compile_the_project ~root_dir ~target)
---
+++ bin/init_cmd.ml
@@ -0,0 +1,50 @@
+open Core
+open Bavar
+
+let rec initialize_new_project ~path ~target ~forced () =
+  if (not forced) && Sys_unix.is_directory_exn path then (
+    eprintf "The '%s' directory already exist! Can't initialize a project.\n"
+      path;
+    exit 1)
+  else
+    let proj_name = Filename.basename path in
+    let config = Project_config.make ~name:proj_name () in
+    let target =
+      let open Option.Let_syntax in
+      let%map target = target in
+
+      match String.rsplit2 ~on:':' target with
+      | Some (mcu, freq) -> sprintf "%s %s" mcu freq
+      | None -> sprintf "%s 1mhz" target
+    in
+
+    entry_to_dir path;
+
+    Core_unix.mkdir_p config.layout.root_dir;
+
+    Out_channel.with_file "LabAvrProject" ~f:(fun ch ->
+        fprintf ch "(name %s)\n" proj_name;
+        Option.iter target ~f:(fprintf ch "(target %s)\n");
+
+        Out_channel.write_all
+          (Filename.concat config.layout.root_dir "main.c")
+          ~data:{|int main(void) {
+  while (1) {
+    // code
+  }
+}
+|});
+
+    printf "Success: initialized project named '%s'\n" proj_name
+
+and entry_to_dir path =
+  Core_unix.mkdir_p path;
+  Core_unix.chdir path
+
+let command =
+  Command.basic ~summary:"initialize a new project"
+    (let%map_open.Command target =
+       flag "-target" (optional string) ~doc:"mcu:freq MCU and frequency values"
+     and force = flag "-f" no_arg ~doc:"force"
+     and path = anon ("path" %: string) in
+     initialize_new_project ~path ~target ~forced:force)
